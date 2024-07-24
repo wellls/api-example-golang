7110b2854c0fc599f84049c9e988dc0d6966b970
Wellington Lima <wellington.map@hotmail.com>
2024-07-24 15:15:08 -0300

    implement create user service

--- internal/service/userservice/user_service.go
+++ internal/service/userservice/user_service.go
@@ -2,14 +2,50 @@ package userservice
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"log/slog"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/wellls/api-example-golang/internal/dto"
+	"github.com/wellls/api-example-golang/internal/entity"
 	"github.com/wellls/api-example-golang/internal/handler/response"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func (s *service) CreateUser(ctx context.Context, u dto.CreateUserDto) error {
+	userExists, err := s.repo.FindUserByEmail(ctx, u.Email)
+	if err != nil {
+		slog.Error("error to search user by email", "err", err, slog.String("package", "userservice"))
+		return err
+	}
+	if userExists != nil {
+		slog.Error("user already exists", slog.String("package", "userservice"))
+		return errors.New("user already exists")
+	}
+
+	passwordEncrypted, err := bcrypt.GenerateFromPassword([]byte(u.Password), 12)
+	if err != nil {
+		slog.Error("error to encrypt password", "err", err, slog.String("package", "userservice"))
+		return errors.New("error to encrypt password")
+	}
+
+	newUser := entity.UserEntity{
+		ID:        uuid.New().String(),
+		Name:      u.Name,
+		Email:     u.Email,
+		Password:  string(passwordEncrypted),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	err = s.repo.CreateUser(ctx, &newUser)
+	if err != nil {
+		slog.Error("error to create user", "err", err, slog.String("package", "userservice"))
+		return err
+	}
+
 	return nil
 }
 
