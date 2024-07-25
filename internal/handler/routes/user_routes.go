a56d563cd35684a93f3a5288b0683a3a9f48e2f5
Wellington Lima <wellington.map@hotmail.com>
2024-07-25 09:24:59 -0300

    feat: protecting routes

--- internal/handler/routes/user_routes.go
+++ internal/handler/routes/user_routes.go
@@ -2,19 +2,23 @@ package routes
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth"
+	"github.com/wellls/api-example-golang/config/env"
 	"github.com/wellls/api-example-golang/internal/handler/userhandler"
 )
 
 func InitUserRoutes(router chi.Router, h userhandler.UserHandler) {
+	router.Post("/user", h.CreateUser)
 	router.Route("/user", func(r chi.Router) {
-		r.Post("/", h.CreateUser)
+		r.Use(jwtauth.Verifier(env.Env.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+
 		r.Patch("/{id}", h.UpdateUser)
 		r.Get("/{id}", h.GetUserByID)
 		r.Delete("/{id}", h.DeleteUser)
 		r.Get("/", h.FindManyUsers)
 		r.Patch("/password/{id}", h.UpdateUserPassword)
 	})
-
 	router.Route("/auth", func(r chi.Router) {
 		r.Post("/login", h.Login)
 	})
