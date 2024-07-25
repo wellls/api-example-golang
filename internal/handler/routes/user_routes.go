9a8611682afb73d98cdd23453a7c0f35249897c3
Wellington Lima <wellington.map@hotmail.com>
2024-07-25 16:08:40 -0300

    feat: create logger middleware

--- internal/handler/routes/user_routes.go
+++ internal/handler/routes/user_routes.go
@@ -4,20 +4,23 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/jwtauth"
 	"github.com/wellls/api-example-golang/config/env"
+	"github.com/wellls/api-example-golang/internal/handler/middleware"
 	"github.com/wellls/api-example-golang/internal/handler/userhandler"
 )
 
 func InitUserRoutes(router chi.Router, h userhandler.UserHandler) {
+	router.Use(middleware.LoggerData)
+
 	router.Post("/user", h.CreateUser)
 	router.Route("/user", func(r chi.Router) {
 		r.Use(jwtauth.Verifier(env.Env.TokenAuth))
 		r.Use(jwtauth.Authenticator)
 
-		r.Patch("/{id}", h.UpdateUser)
-		r.Get("/{id}", h.GetUserByID)
-		r.Delete("/{id}", h.DeleteUser)
-		r.Get("/", h.FindManyUsers)
-		r.Patch("/password/{id}", h.UpdateUserPassword)
+		r.Patch("/", h.UpdateUser)
+		r.Get("/", h.GetUserByID)
+		r.Delete("/", h.DeleteUser)
+		r.Get("/list-all", h.FindManyUsers)
+		r.Patch("/password", h.UpdateUserPassword)
 	})
 	router.Route("/auth", func(r chi.Router) {
 		r.Post("/login", h.Login)
